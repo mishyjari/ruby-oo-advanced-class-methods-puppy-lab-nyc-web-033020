e6d3aff6a0224274c132113b8a459dd1f13abce8
Michelle Frattaroli <mishyjari@outlook.com>
2020-04-01 21:37:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-class 
+class Dog
+  
+  
+  @@all = []
+  
+  def initialize
+    
+    
+    
+    Dog.all << self
+  end
+  
+  def all 
+    @@all
+  end
+  
+  def print_all
+    each dog in @all
+      puts dog
+    end
+  end
+  
+end
